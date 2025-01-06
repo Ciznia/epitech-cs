f08f48203ae939aec408f72b240162aa610f57bf
Ciznia <gabriel.hosquet@epitech.eu>
2025-01-06 15:35:47 +0100

    WIP 1.0.0

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,40 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+} from "vscode-languageclient/node";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+let client: LanguageClient;
+
 export function activate(context: vscode.ExtensionContext) {
+  const serverCommand = "ecsls_run";
+  const serverOptions: ServerOptions = {
+    command: serverCommand,
+    args: [],
+  };
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "epitech-cs" is now active!');
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: "file", language: "c" }],
+    synchronize: {
+      fileEvents: vscode.workspace.createFileSystemWatcher("**/.git"),
+    },
+  };
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('epitech-cs.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from epitech cs!');
-	});
+  client = new LanguageClient(
+    "ecsls",
+    "ECS Language Server",
+    serverOptions,
+    clientOptions
+  );
 
-	context.subscriptions.push(disposable);
+  // Start the client
+  client.start();
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
